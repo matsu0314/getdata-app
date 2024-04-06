09f6020eac709f5572b52cf615d4212f007998ed
donguri <donguri47@gmail.com>
2024-04-06 11:13:15 +0900

    refactor:コードフォーマット

--- modules/csv_shiftJis.js
+++ modules/csv_shiftJis.js
@@ -1,55 +1,59 @@
-const fs = require('fs');
-const iconv = require('iconv-lite');
-const csv = require('csv');
+const fs = require("fs");
+const iconv = require("iconv-lite");
+const csv = require("csv");
 // const read_csv_shiftjis = require('./readCsvShiftjis');
 
-  const write_csv_shiftjis = (path, data) => {
-    return new Promise(async (resolve, reject) => {
-      const data_to_csv = array => {
-        return new Promise((resolve, reject) => {
-          csv.stringify(array, (err, output) => {
-            if (err) {
-              console.log('エラーが発生しました。' + err);
-              resultItem.errMessage.push("CSVの書き込みでエラーが発生しました。");
-              reject(err);
-              return;
-            }
-            resolve(output);
-          });
-        });
-      };
-
-      try {
-        const _csv_data = await data_to_csv(data);
-        // CSVファイルにデータを追記
-        fs.writeFile(path, iconv.encode(_csv_data, 'shift_jis'), {flag: "w"},  err => {
+const write_csv_shiftjis = (path, data) => {
+  return new Promise(async (resolve, reject) => {
+    const data_to_csv = (array) => {
+      return new Promise((resolve, reject) => {
+        csv.stringify(array, (err, output) => {
           if (err) {
-            console.log('エラーが発生しました。' + err);
+            console.log("エラーが発生しました。" + err);
+            resultItem.errMessage.push("CSVの書き込みでエラーが発生しました。");
+            reject(err);
+            return;
+          }
+          resolve(output);
+        });
+      });
+    };
+
+    try {
+      const _csv_data = await data_to_csv(data);
+      // CSVファイルにデータを追記
+      fs.writeFile(
+        path,
+        iconv.encode(_csv_data, "shift_jis"),
+        { flag: "w" },
+        (err) => {
+          if (err) {
+            console.log("エラーが発生しました。" + err);
             resultItem.errMessage.push(err);
             reject(err);
             return;
           }
           resolve();
-        });
-      } catch (err) {
-        reject(err);
-      }
-    });
-  };
+        }
+      );
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-  const csvWrite = async (csvPath, resultObjAry) => {
-    const resultAry = [
-      ['商品コード', 'タイトル', 'サムネイル'],
-    ];
+const csvWrite = async (csvPath, resultObjAry) => {
+  const resultAry = [["スラッグ", "タイトル", "記事ID", "サムネイル名", "URL"]];
 
-    await Promise.all(resultObjAry.map(async (resultObj) => {
-      const { itemCode, fileName } = resultObj;
+  await Promise.all(
+    resultObjAry.map(async (resultObj) => {
+      const { slug, postTitle, postId, thumbName, url } = resultObj;
 
-      resultAry.push([itemCode, fileName, ''])
-    }));
+      resultAry.push([slug, postTitle, postId, thumbName, url]);
+    })
+  );
 
-    await write_csv_shiftjis(csvPath, resultAry);
- 
-  };
+  await write_csv_shiftjis(csvPath, resultAry);
+};
 
-  module.exports = csvWrite;
+module.exports = csvWrite;
